c3df972fb24fae99d10713bff6a939c008999c12
Facundo Leonardelli <facundoleonardelli@gmail.com>
2019-11-15 02:39:01 -0300

    Fix

--- src/main/java/com/mercadolibre/braavos/invoices/kafka/KafkaConfigConsumer.java
+++ src/main/java/com/mercadolibre/braavos/invoices/kafka/KafkaConfigConsumer.java
@@ -31,19 +31,9 @@ public class KafkaConfigConsumer {
 
     @Bean
     public ConsumerFactory<String, EventNotification> consumerFactory() {
-
-        String username = "fsbhoaeu";
-        String password = "DM7fPrkTtyq35aHKSGJoIQBZwVURAZA_";
-
-        String jaasTemplate = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"%s\" password=\"%s\";";
-        String jaasCfg = String.format(jaasTemplate, username, password);
-
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,  bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put("security.protocol", "SASL_SSL");
-        props.put("sasl.mechanism", "SCRAM-SHA-256");
-        props.put("sasl.jaas.config", jaasCfg);
         return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new JsonDeserializer<>(EventNotification.class, objectMapper, false));
     }
 
